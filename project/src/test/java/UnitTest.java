75d81ba5f67f91603fa3fd62700e90e7def81b80
Xerx350 <pr239mail@mail.ru>
2023-04-24 21:22:45 +0300

    tests

--- project/src/test/java/UnitTest.java
+++ project/src/test/java/UnitTest.java
@@ -1,4 +1,6 @@
 //import app.Point;
+import app.Circle;
+import app.Point;
 import app.Task;
 import misc.CoordinateSystem2d;
 import misc.Vector2d;
@@ -13,101 +15,75 @@ import java.util.Set;
  */
 public class UnitTest {
 
-//    /**
-//     * Тест
-//     *
-//     * @param points        список точек
-//     * @param crossedCoords мн-во пересечений
-//     * @param singleCoords  мн-во разности
-//     */
-//    private static void test(ArrayList<Point> points, Set<Vector2d> crossedCoords, Set<Vector2d> singleCoords) {
-//        Task task = new Task(new CoordinateSystem2d(10, 10, 20, 20), points);
-//        task.solve();
-//        // проверяем, что координат пересечения в два раза меньше, чем точек
-//        assert crossedCoords.size() == task.getCrossed().size() / 2;
-//        // проверяем, что координат разности столько же, сколько точек
-//        assert singleCoords.size() == task.getSingle().size();
-//
-//        // проверяем, что все координаты всех точек пересечения содержатся в множестве координат
-//        for (Point p : task.getCrossed()) {
-//            assert crossedCoords.contains(p.getPos());
-//        }
-//
-//        // проверяем, что все координаты всех точек разности содержатся в множестве координат
-//        for (Point p : task.getSingle()) {
-//            assert singleCoords.contains(p.getPos());
-//        }
-//    }
-//
-//
-//    /**
-//     * Первый тест
-//     */
-//    @Test
-//    public void test1() {
-//        ArrayList<Point> points = new ArrayList<>();
-//
-//        points.add(new Point(new Vector2d(1, 1), Point.PointSet.FIRST_SET));
-//        points.add(new Point(new Vector2d(-1, 1), Point.PointSet.FIRST_SET));
-//        points.add(new Point(new Vector2d(-1, 1), Point.PointSet.SECOND_SET));
-//        points.add(new Point(new Vector2d(2, 1), Point.PointSet.FIRST_SET));
-//        points.add(new Point(new Vector2d(1, 2), Point.PointSet.SECOND_SET));
-//        points.add(new Point(new Vector2d(1, 2), Point.PointSet.FIRST_SET));
-//
-//        Set<Vector2d> crossedCoords = new HashSet<>();
-//        crossedCoords.add(new Vector2d(1, 2));
-//        crossedCoords.add(new Vector2d(-1, 1));
-//
-//        Set<Vector2d> singleCoords = new HashSet<>();
-//        singleCoords.add(new Vector2d(1, 1));
-//        singleCoords.add(new Vector2d(2, 1));
-//
-//        test(points, crossedCoords, singleCoords);
-//    }
-//
-//    /**
-//     * Второй тест
-//     */
-//    @Test
-//    public void test2() {
-//        ArrayList<Point> points = new ArrayList<>();
-//
-//        points.add(new Point(new Vector2d(1, 1), Point.PointSet.FIRST_SET));
-//        points.add(new Point(new Vector2d(2, 1), Point.PointSet.FIRST_SET));
-//        points.add(new Point(new Vector2d(2, 2), Point.PointSet.FIRST_SET));
-//        points.add(new Point(new Vector2d(1, 2), Point.PointSet.FIRST_SET));
-//
-//        Set<Vector2d> crossedCoords = new HashSet<>();
-//
-//        Set<Vector2d> singleCoords = new HashSet<>();
-//        singleCoords.add(new Vector2d(1, 1));
-//        singleCoords.add(new Vector2d(2, 1));
-//        singleCoords.add(new Vector2d(2, 2));
-//        singleCoords.add(new Vector2d(1, 2));
-//
-//        test(points, crossedCoords, singleCoords);
-//    }
-//
-//    /**
-//     * Третий тест
-//     */
-//    @Test
-//    public void test3() {
-//        ArrayList<Point> points = new ArrayList<>();
-//
-//        points.add(new Point(new Vector2d(1, 1), Point.PointSet.FIRST_SET));
-//        points.add(new Point(new Vector2d(2, 1), Point.PointSet.SECOND_SET));
-//        points.add(new Point(new Vector2d(2, 2), Point.PointSet.SECOND_SET));
-//        points.add(new Point(new Vector2d(1, 2), Point.PointSet.FIRST_SET));
-//
-//        Set<Vector2d> crossedCoords = new HashSet<>();
-//
-//        Set<Vector2d> singleCoords = new HashSet<>();
-//        singleCoords.add(new Vector2d(1, 1));
-//        singleCoords.add(new Vector2d(2, 1));
-//        singleCoords.add(new Vector2d(2, 2));
-//        singleCoords.add(new Vector2d(1, 2));
-//
-//        test(points, crossedCoords, singleCoords);
-//    }
+    /**
+     * Проверяет, находится ли точка в окружности
+     * @param point точка
+     * @param circle окружность
+     * @return результат проверки
+     */
+    private static boolean isInside(Point point, Circle circle) {
+        return Vector2d.subtract(point.pos, circle.pos).length() <= circle.r;
+    }
+    /**
+     * Тело проверки
+     * @param points исходные данные
+     */
+    private static void test(ArrayList<Point> points) {
+        Task task = new Task(new CoordinateSystem2d(-10, -10, 10, 10), points);
+        task.solve();
+        ArrayList<Circle> circles = task.getCircles();
+
+        int count0 = 0;
+        int count1 = 0;
+        for (Point p: points) {
+            if (isInside(p, circles.get(0))) ++count0;
+            if (isInside(p, circles.get(1))) ++count1;
+        }
+
+        assert count0 >= points.size() / 2 && count1 >= points.size() / 2;
+    }
+
+    /**
+     * Тест 1
+     */
+
+    @Test
+    public void test1() {
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(new Point(new Vector2d(0, 0)));
+        points.add(new Point(new Vector2d(3, 0)));
+        points.add(new Point(new Vector2d(2, 2)));
+        points.add(new Point(new Vector2d(-8, -7)));
+        points.add(new Point(new Vector2d(-3, 2)));
+
+        test(points);
+    }
+    /**
+     * Тест 2
+     */
+    @Test
+    public void test2() {
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(new Point(new Vector2d(-10, -10)));
+        points.add(new Point(new Vector2d(10, 10)));
+        points.add(new Point(new Vector2d(8, 8)));
+        points.add(new Point(new Vector2d(-3, -3)));
+        points.add(new Point(new Vector2d(0, 0)));
+
+        test(points);
+    }
+    /**
+     * Тест 3
+     */
+    @Test
+    public void test3() {
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(new Point(new Vector2d(1, 0)));
+        points.add(new Point(new Vector2d(9, 0)));
+        points.add(new Point(new Vector2d(-3, 2)));
+        points.add(new Point(new Vector2d(-10, -7)));
+        points.add(new Point(new Vector2d(10, 2)));
+
+        test(points);
+    }
 }
